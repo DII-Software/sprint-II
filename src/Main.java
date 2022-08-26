361d269a51e3ce8ba34b376d6775ece2529f3e6b
Faiver <fafevalo99@gmail.com>
2022-08-26 10:39:43 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Empresa epsa = new Empresa();
+        Empresa epsa = new Empresa( );
 
-        epsa.setNombreEmpresa();
+        /*epsa.setNombreEmpresa();
+        epsa.setIdEmpresa();
+        epsa.setDireccionEmpresa();
+        epsa.setNumeroDeTelefono();*/
+
+        epsa.getIdEmpresa();
+
 
 
 
