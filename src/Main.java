31fcca743c5c5fcbf33dda3e255fdcb7849c5b73
Santiagovelas119 <santiagovelas119@gmail.com>
2022-08-26 16:43:09 -0500

    Se agregan atrinutos, metodos, setters y getters de la clase Perfil

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
+
+
 public class Main {
     public static void main(String[] args) {
 
-        Empresa epsa = new Empresa();
-
-        /* epsa.setNombreEmpresa();
-        epsa.setIdEmpresa();
-        epsa.setDireccionEmpresa();
-        epsa.setNumeroDeTelefono();*/
+        Empresa epsa =new Empresa("dllsoftware","calle42","3013098685","9008025");
 
         epsa.getNombreEmpresa();
+        epsa.getDireccionEmpresa();
+        epsa.getNumeroDeTelefono();
+        epsa.getIdEmpresa();
+
+
+       // epsa.setNombreEmpresa();
+       // epsa.setIdEmpresa();
+       // epsa.setDireccionEmpresa();
+       // epsa.setNumeroDeTelefono();
+
+
+
+
 
 
 
--- src/Perfil.java
+++ src/Perfil.java
@@ -1,2 +1,66 @@
 public class Perfil {
+    public int id;
+    public Strig image;
+    public String phone;
+    public String user;
+    public date createdAt;
+    public date UpdatedAt;
+    public Perfil(int id, Strig image, String phone, String user, date createdAt, date updatedAt) {
+        this.id = id;
+        this.image = image;
+        this.phone = phone;
+        this.user = user;
+        this.createdAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+    public Perfil() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Strig getImage() {
+        return image;
+    }
+
+    public void setImage(Strig image) {
+        this.image = image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public date getUpdatedAt() {
+        return UpdatedAt;
+    }
+
+    public void setUpdatedAt(date updatedAt) {
+        UpdatedAt = updatedAt;
+    }
 }
