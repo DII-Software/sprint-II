36d509f679ab4f3565b86465df2ea2babe9ee0f0
Santiagovelas119 <santiagovelas119@gmail.com>
2022-08-25 21:33:44 -0500

    [Update] atributos empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,2 +1,10 @@
 public class Empleado {
+    private long id;
+    private String email;
+    //private Profile profile;
+    //private Enum;
+    //private Enterprise enterprise;
+    //private Transaction[] transaction;
+    //private date updatedAt;
+    //private date createdAt;
 }
