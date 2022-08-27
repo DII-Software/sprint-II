2c53d857f341e4f9dbbaf2cfaa50f09fafadebfe
Santiagovelas119 <santiagovelas119@gmail.com>
2022-08-26 20:40:16 -0500

    [Update] atributos clase employee, getters y setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,40 @@
 import sprint_ii.Empresa;
 
 public class Employee {
-    private long id;
-    private String email;
-    private Perfil perfil;
-    //private Enum;
-    private Empresa enterprise;
-    private sprint_ii.Transaction[] transaction;
-    //private date updatedAt;
-    //private date createdAt;
-}
+    private String nombre;
+    private String correo;
+    private Empresa empresa;
+    //private Enum rol;
+
+    public Employee(String nombre, String correo, Empresa empresa){
+        //Falta crear el archivo enum
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        //
+    }
+    //Getters
+    //Falta el getter del rol del empleado
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    //Setters
+    //Falta el setter del rol del empleado
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+};
+
