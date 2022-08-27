d35440bea538e4af049b63ea03672f43b44bfe8e
Santiagovelas119 <santiagovelas119@gmail.com>
2022-08-26 20:03:37 -0500

    [Update] atributos

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+import sprint_ii.Empresa;
+
+public class Employee {
+    private long id;
+    private String email;
+    private Perfil perfil;
+    //private Enum;
+    private Empresa enterprise;
+    private sprint_ii.Transaction[] transaction;
+    //private date updatedAt;
+    //private date createdAt;
+}
